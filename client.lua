3dce260297d4841b904242e6a2bdde112176df5f
Haroldo <haroldobeyerbacellar@gmail.com>
2020-07-22 00:11:50 -0300

    Initial commit

---
+++ client.lua
@@ -0,0 +1,12 @@
+require 'common'
+local client = clientServer.client
+client.enabled = true
+client.start('127.0.0.1:22122')
+
+function client.connect()
+    print('client: connected to server')
+end
+
+function client.disconnect()
+    print('client: disconnected from server')
+end
---
+++ server.lua
@@ -0,0 +1,12 @@
+require 'common'
+local server = clientServer.server
+server.enabled = true
+server.start('22122')
+
+function server.connect(clientId)
+    print('server: client ' .. clientId .. ' connected')
+end
+
+function server.disconnect(clientId)
+    print('server: client ' .. clientId .. ' disconnected')
+end
