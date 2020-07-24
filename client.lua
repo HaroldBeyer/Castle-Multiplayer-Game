40e5f539813f108f83571b46bab829e5d2af79c5
Haroldo <haroldobeyerbacellar@gmail.com>
2020-07-24 12:19:58 -0300

    AAdding movement

--- client.lua
+++ client.lua
@@ -5,9 +5,13 @@ client.start('127.0.0.1:22122')
 
 local share = client.share
 local home = client.home
+local PLAYER_SPEED = 200
 
 function client.connect()
     print('client: connected to server')
+
+    local player = share.players[client.id]
+    home.x, home.y = player.x, player.y
 end
 
 function client.disconnect()
@@ -32,9 +36,31 @@ end
 function client.draw()
     if client.connected then
         for clientId, player in pairs(share.players) do
-            love.graphics.rectangle('fill', player.x - 20, player.y - 20, 40, 40)
+            local x, y = player.x, player.y
+            if clientId == client.id then
+                x, y = home.x, home.y
+            end
+            love.graphics.rectangle('fill', x - 20, y - 20, 40, 40)
         end
     else
         love.graphics.print('connecting...', 20, 20)
     end
 end
+
+function client.update(dt)
+    if client.connected then
+        if love.keyboard.isDown('left') then
+            home.x = home.x - PLAYER_SPEED * dt
+        end
+        if love.keyboard.isDown('right') then
+            home.x = home.x + PLAYER_SPEED * dt
+        end
+        if love.keyboard.isDown('up') then
+            home.y = home.y - PLAYER_SPEED * dt
+        end
+        if love.keyboard.isDown('down') then
+            home.y = home.y + PLAYER_SPEED * dt
+        end
+    end
+end
+
